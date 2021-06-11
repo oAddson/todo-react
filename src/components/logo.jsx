6060607f98d220f87595e9cdadd0e7f6a6062017
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-06-11 20:24:38 -0300

    header refactor

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../logo.svg';
 import './logo.css';
 
-class Logo extends Component {
-    state = {  }
-    style = {
-        maxWidth: '20px'
-    }
-    render() { 
-        return (
-            <div className="logo">
-                <a href="./">
-                    <img src={logo} alt="Addson Azeredo Coutinho" />
-                </a>
-            </div>
-        );
-    }
+const Logo = () => {
+    return (
+        <div className="logo">
+            <a href="./">
+                <img src={logo} alt="Addson Azeredo Coutinho" />
+            </a>
+        </div>
+    )
 }
- 
+
 export default Logo;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,17 @@
-import React, {Component} from 'react';
-import './navbar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
-class NavBar extends Component {
-    state = { tab: [
-        {id: 1, label: 'Home', url: '.'},
-        {id: 2, label: 'To-do', url: './todo'},
-        {id: 3, label: 'Pomodoro', url: './pomodoro'},
-    ]  }
-    render() {
-        return (
-            <nav className="navbar">
-                <ul>
-                    {this.state.tab.map(item => <li key={item.id} ><a onClick={(e) => this.props.setTab(e, item.id)} href={item.url}> {item.label}</a></li>)}
-                </ul>
-            </nav>
-        );
-    }
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/todo">To-do</Link></li>
+                <li><Link to="/pomodoro">Pomodoro</Link></li>
+            </ul>
+        </nav>
+    );
 }
  
 export default NavBar;
