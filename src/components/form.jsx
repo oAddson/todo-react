d552de905759dd13cab7af5c260b6929ce256dc7
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-06-15 20:25:39 -0300

    to-do input refactor

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
-import './form.css'
-import Input from './input';
+import React from 'react';
+import './form.css';
 
-class Form extends Component {
-    render() {
-        const {name, status} = this.props.values;
-        return (
+const Form = ({children}) => {
+    return (
+        <>
             <form className="create-todo">
-                <fieldset>
-                    <legend>Create to-do</legend>                    
-                    <Input label="Name: " type="text" name="name" id="name" value={name} onChange={this.props.onChange} />
-                    <Input label="Done: " type="checkbox" name="done" id="done" checked={status} onChange={this.props.onChange} />
-                    <div className="button">
-                        <button onClick={this.props.onClick}>+</button>
-                    </div>                    
-                </fieldset>
+                {children}            
             </form>
-        );
-    }
+        </>
+    );
 }
  
 export default Form;
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './input.css'
 
-class Input extends Component {
-
-    render() { 
-        const {name, id, type, src, alt, label, value, checked} = this.props;
-        return (
-            <React.Fragment>
-                <div className="group">
-                    {label !== undefined ? <label htmlFor={name}>{label}</label> : ''}                    
-                    <input type={type} name={name} id={id} src={src} alt={alt} checked={checked} value={value} onChange={this.props.onChange} />
-                </div>
-            </React.Fragment>
-        );
-    }
+const Input = ({name, id, type, src, alt, label, value, checked, onChange}) => {
+    return (
+    <>
+        <div className="group">
+            {label !== undefined ? <label htmlFor={name}>{label}</label> : ''}                    
+            <input type={type} name={name} id={id} src={src} alt={alt} checked={checked} value={value} onChange={onChange}/>
+        </div>
+    </>
+    );
 }
- 
+
 export default Input;
