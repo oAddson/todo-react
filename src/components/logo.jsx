8657d9bcf26b84062459b12667eef4471118363d
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-06-15 20:34:44 -0300

    added link from react router

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import './logo.css';
 
 const Logo = () => {
     return (
         <div className="logo">
-            <a href="./">
+            <Link to="/">
                 <img src={logo} alt="Addson Azeredo Coutinho" />
-            </a>
+            </Link>
         </div>
     )
 }
